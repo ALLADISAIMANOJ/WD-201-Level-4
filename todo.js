351bc8f6cb3108e2f6cd8f7d44e12db94f89a8bc
ALLADISAIMANOJ <86214745+ALLADISAIMANOJ@users.noreply.github.com>
2022-11-18 15:15:00 +0530

    last commit

--- todo.js
+++ todo.js
@@ -1,89 +1,112 @@
-const todoList = () => {
-  all = [];
-  const add = (todoItem) => {
-    all.push(todoItem);
-  };
-  const markAsComplete = (index) => {
-    all[index].completed = true;
-  };
+/* eslint-disable no-undef */
 
-  const overdue = () => {
-    const formattedDate = (d) => {
-      return d.toISOString().split("T")[0];
-    };
+let todoList = require("../todo");
 
-    const dateToday = new Date();
-    const today = formattedDate(dateToday);
+const {
+  all,
+  add,
+  markAsComplete,
+  overdue,
+  dueToday,
+  dueLater,
+  toDisplayableList,
+} = todoList();
 
-    return all.filter((todo) => {
-      if (todo.dueDate < today) return true;
-      else return false;
-    });
-  };
-
-  const dueToday = () => {
-    const formattedDate = (d) => {
-      return d.toISOString().split("T")[0];
-    };
-
-    const dateToday = new Date();
-    const today = formattedDate(dateToday);
-
-    let arr = all.filter((todo) => {
-      if (todo.dueDate == today) return true;
-      else return false;
-    });
-    return arr;
-  };
-
-  const dueLater = () => {
-    const formattedDate = (d) => {
-      return d.toISOString().split("T")[0];
-    };
-
-    const dateToday = new Date();
-    const today = formattedDate(dateToday);
-
-    return all.filter((todo) => {
-      if (todo.dueDate > today) return true;
-      else return false;
-    });
-  };
-
-  const toDisplayableList = (list) => {
-    const formattedDate = (d) => {
-      return d.toISOString().split("T")[0];
-    };
-
-    const dateToday = new Date();
-    const today = formattedDate(dateToday);
-
-    let flag = list[0].dueDate == today ? true : false;
-    const strArr = list.map((todo) => {
-      if (flag == true) {
-        if (todo.completed == true) return "[x] " + todo.title;
-        else return "[ ] " + todo.title;
-      } else {
-        if (todo.completed == true)
-          return "[x] " + todo.title + " " + todo.dueDate;
-        else return "[ ] " + todo.title + " " + todo.dueDate;
-      }
-    });
-
-    let str = strArr.join("\n");
-
-    return str;
-  };
-
-  return {
-    all,
-    add,
-    markAsComplete,
-    overdue,
-    dueToday,
-    dueLater,
-    toDisplayableList,
-  };
+const dateToday = new Date();
+const formattedDate = (d) => {
+  return d.toISOString().split("T")[0];
 };
 
-module.exports = todoList;
+const today = formattedDate(dateToday);
+
+describe("Todo List Test Suite", () => {
+  beforeAll(() => {
+    const today = new Date();
+    const initDay = 60 * 60 * 24 * 1000;
+    const initTodos = [
+      {
+        title: "Submit Record",
+        completed: false,
+        dueDate: new Date(today.getTime() - 2 * initDay).toLocaleDateString(
+          "en-CA"
+        ),
+      },
+      {
+        title: "propose a Girlfriend",
+        completed: false,
+        dueDate: new Date(today.getTime() - 4 * initDay).toLocaleDateString(
+          "en-CA"
+        ),
+      },
+      {
+        title: "Submit Record",
+        completed: false,
+        dueDate: new Date(today.getTime() - 2 * initDay).toLocaleDateString(
+          "en-CA"
+        ),
+      },
+      {
+        title: "Participate in today Codechef contest",
+        completed: false,
+        dueDate: new Date().toLocaleDateString("en-CA"),
+      },
+      {
+        title: "Buy car",
+        completed: false,
+        dueDate: new Date().toLocaleDateString("en-CA"),
+      },
+      {
+        title: "Pay college fee",
+        completed: false,
+        dueDate: new Date(today.getTime() + 2 * initDay).toLocaleDateString(
+          "en-CA"
+        ),
+      },
+    ];
+    initTodos.map(add);
+  });
+  test("Should add a new todo", () => {
+    const initItems = all.length;
+
+    add({
+      title: "A test item",
+      completed: false,
+      dueDate: new Date().toLocaleDateString("en-CA"),
+    });
+
+    expect(all.length).toEqual(initItems + 1);
+  });
+
+  test("Should mark a todo as complete(Mark as read)", () => {
+    expect(all[0].completed).toEqual(false);
+    markAsComplete(0);
+    expect(all[0].completed).toEqual(true);
+  });
+
+  test("Should retrieve overdue items", () => {
+    overDueTodos = overdue();
+    expect(
+      overDueTodos.every((todo) => {
+        return todo.dueDate < today;
+      })
+    ).toBe(true);
+  });
+
+  test("Should retrieve due today items", () => {
+    dueTodayTodos = dueToday();
+    expect(
+      dueTodayTodos.every((todo) => {
+        return todo.dueDate === today;
+      })
+    ).toBe(true);
+  });
+
+  test("Should retrieve due later items", () => {
+    dueLaterTodos = dueLater();
+    expect(
+      dueLaterTodos.every((todo) => {
+        return todo.dueDate > today;
+      })
+    ).toBe(true);
+  });
+});
